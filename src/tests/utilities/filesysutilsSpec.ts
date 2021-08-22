37295254e989c7a10ceb7508ec91f60f181f37f0
tomhettinger <ssjsonic1@gmail.com>
2021-08-22 13:54:12 -0700

    fix unit tests for fileexists

--- src/tests/utilities/filesysutilsSpec.ts
+++ src/tests/utilities/filesysutilsSpec.ts
@@ -2,11 +2,11 @@ import {createDir, fileExists} from '../../utilities/filesysutils';
 
 describe("Test suite for fileExists", () => {
     it("Checks that true returns when file is present", () => {
-        expect(fileExists('../resources/cats1.jpg')).toBe(true);
+        expect(fileExists( './resources/tests/cats1.jpg')).toBe(true);
     });
 
-    it("Check that false is returned when file is missing", () => {
-        expect(fileExists('../resources/cats2.jpg')).toBe(false);
+    it("Checks that true returns when file is present", () => {
+        expect(fileExists( './resources/tests/cats2.jpg')).toBe(false);
     });
 
 });
